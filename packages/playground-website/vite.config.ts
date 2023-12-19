dcb905ad3671087f9c717e7d1eff5ac8800ebcf0
Travis Prescott <trpresco@microsoft.com>
2023-12-19 12:38:52 -0800

    Revert base change to vite config.

--- packages/playground-website/vite.config.ts
+++ packages/playground-website/vite.config.ts
@@ -2,22 +2,21 @@ import { definePlaygroundViteConfig } from "@typespec/playground/vite";
 import { defineConfig } from "vite";
 
 const githubUrl = `https://github.com/tjprescott/typespec-pydantic`;
-const customConfig = definePlaygroundViteConfig({
-  defaultEmitter: "typespec-pydantic",
-  libraries: ["@typespec/compiler", "typespec-pydantic"],
-  samples: {
-    "My sample": {
-      filename: "samples/basic.tsp",
-      preferredEmitter: "typespec-pydantic",
+const config = defineConfig(
+  definePlaygroundViteConfig({
+    defaultEmitter: "typespec-pydantic",
+    libraries: ["@typespec/compiler", "typespec-pydantic"],
+    samples: {
+      "My sample": {
+        filename: "samples/basic.tsp",
+        preferredEmitter: "typespec-pydantic",
+      },
     },
-  },
-  links: {
-    githubIssueUrl: `${githubUrl}/issues`,
-    documentationUrl: `${githubUrl}/blob/master/README.md`,
-  },
-});
-// old value "./" for local testing
-customConfig.base = githubUrl;
-const config = defineConfig(customConfig);
+    links: {
+      githubIssueUrl: `${githubUrl}/issues`,
+      documentationUrl: `${githubUrl}/blob/master/README.md`,
+    },
+  }),
+);
 
 export default config;
